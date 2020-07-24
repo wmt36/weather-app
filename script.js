3d9bf8c4993eb6a7f769463352275b505a4cf78b
wmt36 <wmt36@Williams-iMac.local>
2020-07-23 22:14:47 -0600

    updated version of the javascripte

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
     //pulling the needed variable from the HTML
-    var input = document.getElementById("search");
+    var input = document.querySelector("search");
     var fiveDay = document.getElementById("dayAhead");
     var currentCity = document.getElementById("currentcity");
     var queryParams = "d21c600165a9b27ec27d8cad2ffbba4a";
-    var qURL = "http://api.openweathermap.org/data/2.5/weather?" +
+    var qURL = "https://api.openweathermap.org/data/2.5/weather?" +
     "q=denver&appid=" + queryParams;
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" +
     "q=Denver&APPID=" + queryParams;
+
+    /**function renderLast(){   
+     * $(".cities")html("<ul>")
+     * input.textcontent = value
+     * value.append("<li>")
+     * var city = localStorage.getItem("city");
+     * }*/
 
 
     /**allows the person access to view any state current weather by using the API key 
@@ -21,10 +28,6 @@ var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +
             .trim();
 
     }
-
-function updatePagew(){
-
-}
 
 
 
@@ -44,27 +47,35 @@ function updatePagew(){
                 console.log(qURL);
                 console.log("----------");
                 
-                
+                //calling the current weather
 
-                var tempF = (response.main.temp - (293.64) * 1.80 + 32);
+                var tempF = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
 
                 $(".city").html("<h3>" + response.name + "</h3>");
-                $(".wind").text("Wind Speed: " + response.wind.speed);
-                $(".humidity").text("Humidity: " + response.main.humidity);
-                $(".temp").text("Temperature: " + tempF);
-                $(".uv").text("Uv Index: " + response.weather.description);
+                $(".wind").html("<strong> Wind Speed: </strong>" + response.wind.speed);
+                $(".humidity").html("<strong> Humidity: </strong>" + response.main.humidity);
+                $(".temp").html("<strong> Temperature: </strong>" + tempF);
+                $(".uv").html("<strong> Uv Index: </strong>" + response.weather.description);
                 console.log(response);
 
-
+//calling the forcast
                 $.ajax({
                     url: queryURL, 
                     method: "GET"
+                }) 
+                .then(function (forecast) {
+                    console.log(forecast)
+                    // for (var i = 0; i < forecast.list.length; i++){
+                    //     if(forecast.list[i].dt_txt)
+                    // }
+                    
                 })
                 console.log(queryURL);
+                
 
                 $(".cardheader").html("<h3>" + response.name + "</h3>");
                 $(".fwind").text("Wind Speed: " + response.wind.speed);
-                $(".ftemp").text("Temp: " +response.main.temp);
+                $(".ftemp").text("Temp: " + tempF);
                 $(".fhumidity").text("Humidity: " +response.main.humidity);
                 
 
